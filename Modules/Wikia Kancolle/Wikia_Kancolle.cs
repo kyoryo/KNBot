e0c89b73c461635d51ec4c006b57bc2663dc0937
kyoryo <franz.rio19@gmail.com>
2015-12-25 06:28:10 +0700

    add wikiakancolle indev

---
+++ Modules/Wikia Kancolle/Wikia_Kancolle.cs
@@ -0,0 +1,26 @@
+﻿namespace Combot.Modules.Plugins
+{
+    public class Wikia_Kancolle : Module
+    {
+        public override void Initialize()
+        {
+            Bot.CommandReceivedEvent += HandleCommandEvent;
+        }
+
+        public override void ParseCommand(CommandMessage command)
+        {
+            Command foundCommand = Commands.Find(c => c.Triggers.Contains(command.Command));
+
+            switch (foundCommand.Name)
+            {
+                case "Wikia Kancolle";
+                    WikiaKancolle(command);
+                    break;
+            }
+        }
+        private void WikiaKancolle(CommandMessage command)
+        {
+
+        }
+    }
+}
